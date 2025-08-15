06bf2bb357b35ba48fd596d69a2fa75d89697e2d
Cha Jooyeong <75120438+jooo0922@users.noreply.github.com>
2025-08-15 18:47:28 +0900

    refactor: implement class for skia ganesh gpu backend management

---
+++ shared/render/SkiaGanesh.cpp
@@ -0,0 +1,79 @@
+#include "SkiaGanesh.h"
+#include <android/log.h>  // 로그 출력을 위한 헤더
+#include <GLES3/gl3.h> // OpenGL ES 3.0 API 사용
+
+/**
+ * GrDirectContext 및 SkSurface 생성 함수.
+ * -> imageInfo (즉, render target 크기)가 유효할 때 한번 생성하거나, 크기가 변경되었으면 재생성함.
+ */
+bool SkiaGanesh::setupSkiaSurface(int width, int height) {
+  if (width <= 0 || height <= 0) {
+    __android_log_print(ANDROID_LOG_ERROR, "NativeSampleModule", "Invalid image size");
+    return false;
+  }
+
+  // GrDirectContext 생성 (내부적으로 현재 스레드에 바인딩된 egl context 사용)
+  if (!m_pGrContext) {
+    auto interface = GrGLMakeNativeInterface();
+    m_pGrContext = GrDirectContexts::MakeGL(interface);
+    if (!m_pGrContext) {
+      __android_log_print(ANDROID_LOG_ERROR, "NativeSampleModule", "Failed to create GrDirectContext");
+      return false;
+    }
+  }
+
+  // 기존 SkSurface 크기가 현재 이미지 크기(메타데이터)와 맞지 않을 경우 재생성하도록 nullptr 초기화
+  if (m_pSkiaSurface) {
+    SkImageInfo currentInfo = m_pSkiaSurface->imageInfo();
+    if (currentInfo.width() != width || currentInfo.height() != height) {
+      m_pSkiaSurface = nullptr;
+    }
+  }
+
+  // SkSurface (재)생성
+  if (!m_pSkiaSurface) {
+    // 1. OpenGL ES 컨텍스트에 바인딩된 default framebuffer(fbo id == 0) 설정을 GrGLFramebufferInfo 로 구성.
+    GrGLFramebufferInfo fboInfo;
+    fboInfo.fFBOID = (GrGLuint)0;
+    fboInfo.fFormat = GL_RGBA8;
+
+    // 2. OpenGL ES 컨텍스트에 바인딩된 default framebuffer 를 감싸는 skia 의 render target (interface) 생성.
+    GrBackendRenderTarget backendRT = GrBackendRenderTargets::MakeGL(
+      width,         // render target width
+      height,        // render target height
+      0,             // sampleCount (멀티샘플링 안 함)
+      0,             // 스텐실 비트 수 (없다면 0)
+      fboInfo        // OpenGL 기본 FBO 정보
+    );
+
+    // 3. WrapBackendRenderTarget 을 사용하여 default framebuffer 기반 SkSurface 를 생성.
+    m_pSkiaSurface = SkSurfaces::WrapBackendRenderTarget(
+      m_pGrContext.get(),                         // 이미 생성된 GrDirectContext
+      backendRT,
+      kBottomLeft_GrSurfaceOrigin,              // OpenGL은 좌하단 기준
+      SkColorType::kRGBA_8888_SkColorType,      // SkImageInfo의 color type (예, kN32_SkColorType)
+      nullptr,                                  // 컬러 공간 (필요에 따라 지정)
+      nullptr                                   // SkSurfaceProps (필요에 따라)
+    );
+
+    if (!m_pSkiaSurface) {
+      __android_log_print(ANDROID_LOG_ERROR, "NativeSampleModule", "Failed to create SkSurface from backend render target");
+      return false;
+    }
+  }
+
+  return true;
+};
+
+void SkiaGanesh::flush() {
+  if (m_pGrContext) {
+    // Skia 내부 command queue 에 쌓인 현재 프레임까지 요청된 모든 draw operation 들을 GPU 로 전송하여 실행 요청
+    m_pGrContext->flush();
+  }
+};
+
+void SkiaGanesh::destroy() {
+  // ganesh gpu 백엔드 관련 전역 객체 자원 해제
+  m_pGrContext = nullptr;
+  m_pSkiaSurface = nullptr;
+};
---
+++ shared/render/SkiaGanesh.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <core/SkSurface.h>
+#include <core/SkColorSpace.h>
+#include <gpu/ganesh/SkSurfaceGanesh.h>
+#include <gpu/ganesh/GrDirectContext.h>
+#include <gpu/ganesh/GrBackendSurface.h>
+#include <gpu/ganesh/gl/GrGLBackendSurface.h>
+#include <gpu/ganesh/gl/GrGLDirectContext.h>
+#include <gpu/ganesh/gl/GrGLInterface.h>
+
+class SkiaGanesh
+{
+public:
+  // ganesh gpu 백엔드 기반 SkSurface 생성 함수
+  bool setupSkiaSurface(int width, int height);
+  void flush();
+  void destroy();
+
+public:
+  SkCanvas* canvas() const { return m_pSkiaSurface ? m_pSkiaSurface->getCanvas() : nullptr; };
+  sk_sp<SkSurface> surface() const { return m_pSkiaSurface; };
+
+private:
+  // ganesh gpu 백엔드 관련 전역 객체 (skia 버전 스마트 포인터(std::shared_ptr 과 유사)로 관리)
+  sk_sp<GrDirectContext> m_pGrContext;
+  sk_sp<SkSurface> m_pSkiaSurface;
+};
