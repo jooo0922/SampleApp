d432cc07f2130ae69c74df815e84c2d4fa7ebe96
Cha Jooyeong <75120438+jooo0922@users.noreply.github.com>
2025-08-18 23:59:57 +0900

    feat: add TurboModule specs for video decoding

--- specs/NativeSampleModule.ts
+++ specs/NativeSampleModule.ts
@@ -2,6 +2,8 @@ import {TurboModule, TurboModuleRegistry} from 'react-native';
 
 export interface Spec extends TurboModule {
   readonly reverseString: (input: string) => string;
+  readonly startDecoding: (filePath: string) => void;
+  readonly stopDecoding: () => void;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('NativeSampleModule');
